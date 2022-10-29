631f57abb7fc675dfeb280925bd8dce08d9eb8c8
Refazul <refazul.refat@yahoo.com>
2022-10-29 22:39:29 +0600

    mysqlize

--- atomichash/settings.py
+++ atomichash/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,10 +76,17 @@ WSGI_APPLICATION = 'atomichash.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE'),
+        'USER': os.environ.get('MYSQL_USER'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 3306,
+        'OPTIONS': {
+            'init_command': 'SET default_storage_engine=INNODB'
+        }
     }
-}
+} 
 
 
 # Password validation
